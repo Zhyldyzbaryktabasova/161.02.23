ad50487a31f6358567e8430ec73a4cd1db14aa86
Zhyldyzbaryktabasova <zhyldyz.baryktabasovach@gmail.com>
2023-02-17 05:11:57 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+//let example1 = [1,2,3,4,5,6,7,8,9,];
+//let example2 = [[1,2,3],[4,5,6],[7,8,9]];
+
+//console.log(example1.length);
+//console.log(example2.length);
+
+//console.log(example1[2]);
+//console.log(example2[2]);
+//console.log(example1[4]);
+//console.log (example1[7]);
+//console.log(example2[2]);
+
+//console.log(example2[1][1]);
+
+//let test = example2[2];
+//console.log(test[1]);
+
+
+//for (let i = 0; i <example1.length; i++ ) {
+  //console.log(example1[i]);
+//}
+//for (let i = 0; i <example2.length;i++){
+  //for (let j=0; j <example2[i].length; j++) {
+    //console.log(example2[i][j]);
+  //}
+ //}
+ 
+ 
+ let example3 = [4,1,7,10,11,10,33];
+ if (example3.includes(6)) {
+   console.log("Yes");
+ }
+ else {
+  console.log("No")
+ }
