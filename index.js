8476debb5495da8835a3a17199a29918db595ae7
Zhyldyzbaryktabasova <zhyldyz.baryktabasovach@gmail.com>
2023-02-17 05:24:03 +0300

    "a","b'',"c''.

--- index.js
+++ index.js
@@ -26,10 +26,28 @@
  //}
  
  
- let example3 = [4,1,7,10,11,10,33];
- if (example3.includes(6)) {
-   console.log("Yes");
- }
- else {
-  console.log("No")
- }
+ //let example3 = [4,1,7,10,11,10,33];
+ //if (example3.includes(6)) {
+   //console.log("Yes");
+ //}
+ //else {
+  //console.log("No")
+ //}
+
+ //let example4 = [5,4,1,55,1,0,2];
+ //let example5 = [3,11,4,15,1,2,77];
+  //for (let i 0; i < example4.lenght: i++ ) {
+
+ //}
+  //if(example5.includes(example4[4])) {
+    //console.log(example4[i]);
+  //}
+  
+  let example6 = ["a","c","e","d","b"];
+  example6.reverse();
+  console.log(example6);
+
+  example6.sort();
+  console.log(example6);
+
+  
